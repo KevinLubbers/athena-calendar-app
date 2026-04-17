46f2c8b5b52296e90d9ee3fbdc6ccd2cb5572fe4
KevinLubbers <kelubbers@gmail.com>
2026-04-16 23:05:04 -0400

    (ui) added checkbox filters and cenetered the grid icons - added light/dark to login/register pages - still working on colors for all the different filters

--- resources/views/livewire/calendar.blade.php
+++ resources/views/livewire/calendar.blade.php
@@ -52,12 +52,42 @@ new class extends Component {
     </p>
 
 <div class="overflow-auto">
-    <!-- Controls -->
     <div class="flex items-center gap-2 mb-4">
         <button wire:click="prevYear">←</button>
         <span class="font-bold text-lg">{{ $year }}</span>
         <button wire:click="nextYear">→</button>
     </div>
+    <div class="flex flex-row flex-wrap">
+        <div class="flex items-center ml-4 gap-2 mb-4">
+            <x-checkbox wire:model="period" />
+            <x-label for="period" value="Show Period" />
+        </div>
+        <div class="flex items-center ml-4 gap-2 mb-4">
+            <x-checkbox wire:model="fertility"  />
+            <x-label for="fertility" value="Show Fertility" />
+        </div>
+        <div class="flex items-center ml-4 gap-2 mb-4">
+            <x-checkbox wire:model="sex"  />
+            <x-label for="sex" value="Show Sexual Activity" />
+        </div>
+        <div class="flex items-center ml-4 gap-2 mb-4">
+            <x-checkbox wire:model="orgasms" />
+            <x-label for="orgasms" value="Show Orgasms" />
+        </div>
+        <div class="flex items-center ml-4 gap-2 mb-4">
+            <x-checkbox wire:model="medication"/>
+            <x-label for="medication" value="Show Medication" />
+        </div>
+        <div class="flex items-center ml-4 gap-2 mb-4">
+            <x-checkbox wire:model="pregnancy" />
+            <x-label for="pregnancy" value="Show Pregnancy" />
+        </div>
+        <div class="flex items-center ml-4 gap-2 mb-4">
+            <x-checkbox wire:model="showNone" />
+            <x-label for="showNone" value="Show None" />
+        </div>
+    </div>
+
 
     <table class="table-auto w-full text-sm">
         <thead>
@@ -75,11 +105,13 @@ new class extends Component {
         <tbody>
             @foreach ($days as $day)
                 <tr>
-                    <td class="p-2 font-bold">{{ $day }}</td>
+                    <td class="p-2 font-bold text-center">{{ $day }}</td>
 
                     @foreach ($months as $month)
-                        <td class="text-center">
+                        <td class="text-center align-middle">
                             @if ($day <= $month->daysInMonth)
+                                <div class="mx-auto w-4 h-4 bg-green-600 rounded-full"></div>
+                                <!-- bg-blue-500 bg-red-800 bg-purple-800 -->
                             @endif
                         </td>
                     @endforeach
